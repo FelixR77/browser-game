91ead1aedc665c0d0a97ae96600314a26d0d1257
FelixR77 <felix.romero75@gmail.com>
2025-10-02 14:33:57 -0500

    updated square identifier

--- app.js
+++ app.js
@@ -12,15 +12,25 @@
 
 /*------------------------ Cached Element References ------------------------*/
 const buttons = document.querySelectorAll('.grid-square');
-
-
+const start = document.querySelector('#start') 
+let current = start
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
-
+document.addEventListener('keydown', (event) => {
+  switch(event.key) {
+    case "ArrowDown":
+        
+        console.log("moving up")
+  }
+   console.log(event.key)
+})
+ 
 
 
 /*-------------------------------- Functions --------------------------------*/
+buttons.forEach((gridSquare, index) => {
+    gridSquare.setAttribute(`data-expo`, `${index + 1}`);
+    console.log(gridSquare)
+})
